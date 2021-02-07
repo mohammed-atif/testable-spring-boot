326deb5138b182654c6efc07218ad9d9fd2acf5e
Mohammed Atif <atif.mohammed326@gmail.com>
2021-02-07 12:23:14 +0530

    Project setup

---
+++ src/test/java/com/atif/medium/tsb/TestableSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atif.medium.tsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestableSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
